352940c1676a6848e65d671ed1cab194254f0286
Ludovic Claude <ludovic.claude54@gmail.com>
2015-10-26 10:08:06 +0100

    Copy jar to new location

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 sbt assembly
-cp target/scala-2.11/workflow-assembly-0.1.jar ../docker-containers/hbp_federation/workflow/downloads/workflow.jar
-
+cp target/scala-2.11/workflow-assembly-0.1.jar ../docker-containers/mip_federation/workflow/downloads/workflow.jar
