e635a5238a0b9e0a168fce30fd47e547b5876a16
Ludovic Claude <ludovic.claude54@gmail.com>
2015-11-18 12:00:00 +0100

    Cleanup

--- dev-env/post-job.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cat job.json | sed "s/001/$(date +'%s')/" | curl -XPUT -H "Content-Type: application/json" -H "Accept: application/json" --data @- localhost:8087/job
--- dev-env/results.sql
+++
@@ -1,2 +0,0 @@
-
-INSERT INTO result_box_stats VALUES ('001', 'local', 1, 0.1, 0.22, 0.5, 0.77, 1.0);
