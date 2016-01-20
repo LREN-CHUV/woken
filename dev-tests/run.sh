56d7265655f2ec6811b1b705f892bb843b735b1d
Ludovic Claude <ludovic.claude54@gmail.com>
2016-01-20 11:56:02 +0100

    Add tests to ensure that the containers starts without problems

--- dev-tests/run.sh
+++ dev-tests/run.sh
@@ -14,6 +14,20 @@ get_script_dir () {
 
 ROOT_DIR="$(get_script_dir)"
 
+used_ports=$(sudo /bin/sh -c "lsof -iTCP -sTCP:LISTEN -P -n | grep -E ':(8087|5432|2181|2888|3888|5050|5051|4400)'" || true)
+
+if [ "$used_ports" != "" ]; then
+  echo "Some applications already use the ports required by this set of applications. Please close them."
+  echo -n "$used_ports"
+  echo
+  exit 1
+fi
+
+if pgrep -lf sshuttle > /dev/null ; then
+  echo "sshuttle detected. Please close this program as it messes with networking and prevents Docker links to work"
+  exit 1
+fi
+
 echo "Starting the Mesos environment and the workflow application..."
 
 cp $ROOT_DIR/../target/scala-2.11/workflow-assembly-0.1.jar workflow/lib/workflow.jar
