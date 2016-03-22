474a903830fb1c130ee74141cc1718cfeab67e15
Ludovic Claude <ludovic.claude54@gmail.com>
2016-03-22 18:47:53 +0100

    Add more tests

---
+++ dev-tests/query-linear-regression-mcmg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Linear regression with many covariables and many groupings
+
+http -v --timeout 180 POST localhost:8087/mining \
+         variables:='[{"code":"rightsmcsupplementarymotorcortex"}]' \
+         grouping:='[{"code":"AGE"},{"code":"ptgender"}]' \
+         covariables:='[{"code":"leftmprgprecentralgyrusmedialsegment"},{"code":"rightporgposteriororbitalgyrus"},{"code":"rightaorganteriororbitalgyrus"}]' \
+         filters:='[]' \
+         algorithm='linearRegression'
