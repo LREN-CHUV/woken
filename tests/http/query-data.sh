a2bf0973087226cd2665e7915749c9929519b42d
Gabriel Ciuloaica <gciuloaica@gmail.com>
2018-01-21 20:45:47 +0200

    added user into payload to all http tests

--- tests/http/query-data.sh
+++ tests/http/query-data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 http -v --verify=no -a admin:WoKeN --timeout 180 POST http://localhost:8087/mining/job \
+         user:='{"code":"user1"}' \
          variables:='[]' \
          grouping:='[]' \
          covariables:='[{"code":"\"cognitive_task2\""},{"code":"\"score_test1\""}]' \
--- tests/http/query-summary.sh
+++ tests/http/query-summary.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 http -v --verify=no -a admin:WoKeN --timeout 180 POST http://localhost:8087/mining/job \
+         user:='{"code":"user1"}' \
          variables:='[{"code":"stress_before_test1"}]' \
          grouping:='[]' \
          covariables:='[{"code":"score_test1"}]' \
