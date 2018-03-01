0d4073f8298c670672a73554014703d9b2ba6426
Ludovic Claude <ludovic.claude54@gmail.com>
2018-03-01 19:37:05 +0100

    Migrate conversion of FilterRules to sql where to woken-messages

--- src/main/scala/ch/chuv/lren/woken/core/features/Queries.scala
+++ src/main/scala/ch/chuv/lren/woken/core/features/Queries.scala
@@ -19,88 +19,14 @@ package ch.chuv.lren.woken.core.features
 
 import ch.chuv.lren.woken.messages.query.Query
 import ch.chuv.lren.woken.messages.query.filters._
+import ch.chuv.lren.woken.messages.query.filters.FilterRule._
 import ch.chuv.lren.woken.messages.variables.{ FeatureIdentifier, VariableId }
-import org.postgresql.core.Utils
 
 case class QueryOffset(start: Int, count: Int) {
   def end: Int = start + count
 }
 
 object Queries {
-  private val numberRegex = "[-+]?\\d+(\\.\\d+)?".r
-
-  implicit class SqlStrings(val s: String) extends AnyVal {
-
-    def safeValue: String =
-      if (numberRegex.pattern.matcher(s).matches())
-        if (s.startsWith("+"))
-          s.substring(1)
-        else s
-      else {
-        val sb = new java.lang.StringBuilder("'")
-        Utils.escapeLiteral(sb, s, false)
-        sb.append("'")
-        sb.toString
-      }
-
-    def identifier: String = {
-      val sb = new java.lang.StringBuilder()
-      Utils.escapeIdentifier(sb, s)
-      sb.toString
-    }
-  }
-
-  implicit class FilterRuleToSql(val rule: FilterRule) extends AnyVal {
-
-    def withAdaptedFieldName: FilterRule = rule match {
-      case c: CompoundFilterRule =>
-        CompoundFilterRule(c.condition, c.rules.map(_.withAdaptedFieldName))
-      case s: SingleFilterRule =>
-        s.copy(field = s.field.toLowerCase().replaceAll("-", "_").replaceFirst("^(\\d)", "_$1"))
-    }
-
-    def toSqlWhere: String = rule match {
-      case c: CompoundFilterRule =>
-        c.rules
-          .map {
-            case r: SingleFilterRule    => r.toSqlWhere
-            case sc: CompoundFilterRule => s"(${sc.toSqlWhere})"
-          }
-          .mkString(s" ${c.condition.toString} ")
-      case s: SingleFilterRule =>
-        s.operator match {
-          case Operator.equal          => s"${s.field.identifier} = ${s.value.head.safeValue}"
-          case Operator.notEqual       => s"${s.field.identifier} != ${s.value.head.safeValue}"
-          case Operator.less           => s"${s.field.identifier} < ${s.value.head.safeValue}"
-          case Operator.greater        => s"${s.field.identifier} > ${s.value.head.safeValue}"
-          case Operator.lessOrEqual    => s"${s.field.identifier} <= ${s.value.head.safeValue}"
-          case Operator.greaterOrEqual => s"${s.field.identifier} >= ${s.value.head.safeValue}"
-          case Operator.in =>
-            s"${s.field.identifier} IN (${s.value.map(_.safeValue).mkString(",")})"
-          case Operator.notIn =>
-            s"${s.field.identifier} NOT IN (${s.value.map(_.safeValue).mkString(",")})"
-          case Operator.between =>
-            s"${s.field.identifier} BETWEEN ${s.value.head.safeValue} AND ${s.value.last.safeValue}"
-          case Operator.notBetween =>
-            s"${s.field.identifier} NOT BETWEEN ${s.value.head.safeValue} AND ${s.value.last.safeValue}"
-          case Operator.beginsWith =>
-            s"${s.field.identifier} LIKE ${(s.value.head + "%").safeValue}"
-          case Operator.notBeginsWith =>
-            s"${s.field.identifier} NOT LIKE ${(s.value.head + "%").safeValue}"
-          case Operator.contains =>
-            s"${s.field.identifier} LIKE ${("%" + s.value.head + "%").safeValue}"
-          case Operator.notContains =>
-            s"${s.field.identifier} NOT LIKE ${("%" + s.value.head + "%").safeValue}"
-          case Operator.endsWith => s"${s.field.identifier} LIKE ${("%" + s.value.head).safeValue}"
-          case Operator.notEndsWith =>
-            s"${s.field.identifier} NOT LIKE ${("%" + s.value.head).safeValue}"
-          case Operator.isEmpty    => s"'COALESCE(${s.field.identifier}, '') = ''"
-          case Operator.isNotEmpty => s"'COALESCE(${s.field.identifier}, '') != ''"
-          case Operator.isNull     => s"${s.field.identifier} IS NULL"
-          case Operator.isNotNull  => s"${s.field.identifier} IS NOT NULL"
-        }
-    }
-  }
 
   // TODO: add support for GroupId as feature
   implicit class QueryEnhanced(val query: Query) extends AnyVal {
