9f93273aee4a0fb8cc102143c9e0841f0ae0f56b
Ludovic Claude <ludovic.claude54@gmail.com>
2018-03-02 12:28:30 +0100

    Enable strace for debugging

--- docker/runner/woken.sh
+++ docker/runner/woken.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -e
 
-exec java -javaagent:/opt/woken/aspectjweaver.jar \
+JAVA="java"
+
+if [ -n "$TRACE" ]; then
+  apk add --update --no-cache strace
+  JAVA="strace java"
+fi
+
+exec ${JAVA} -javaagent:/opt/woken/aspectjweaver.jar \
           -Djava.library.path=/lib \
           -Dconfig.file=/opt/woken/config/application.conf \
           -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
