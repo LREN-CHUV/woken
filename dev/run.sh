582682eaab82b5e2eae52fa0ec9bd9708d8b7686
Ludovic Claude <ludovic.claude54@gmail.com>
2018-03-15 13:24:24 +0100

    Development environment

---
+++ dev/run.sh
@@ -0,0 +1,118 @@
+#!/usr/bin/env bash
+
+#
+# Start the environment for Woken: Mesos, Chronos, Postgres database with data, Woken validation, Portal frontend.
+#
+# Option:
+#   --no-validation: do not start Woken validation
+#   --no-frontend: do not start the frontend
+#
+
+set -e
+
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     cd -P "$( dirname "$SOURCE" )"
+     pwd
+}
+
+cd "$(get_script_dir)"
+
+validation=1
+frontend=1
+for param in "$@"
+do
+  if [ "--no-frontend" == "$param" ]; then
+    frontend=0
+    echo "INFO: --no-frontend option detected !"
+  fi
+  if [ "--no-validation" == "$param" ]; then
+    validation=0
+    echo "INFO: --no-validation option detected !"
+  fi
+done
+
+if pgrep -lf sshuttle > /dev/null ; then
+  echo "sshuttle detected. Please close this program as it messes with networking and prevents Docker links to work"
+  exit 1
+fi
+
+if groups "$USER" | grep &>/dev/null '\bdocker\b'; then
+  DOCKER="docker"
+  DOCKER_COMPOSE="docker-compose"
+else
+  DOCKER="sudo docker"
+  DOCKER_COMPOSE="sudo docker-compose"
+fi
+
+trap '$DOCKER_COMPOSE rm -f' SIGINT SIGQUIT
+
+echo "Remove old running containers (if any)..."
+$DOCKER_COMPOSE kill
+$DOCKER_COMPOSE rm -f
+
+echo "Deploy a Postgres server and wait for it to be ready..."
+$DOCKER_COMPOSE up -d db zookeeper
+$DOCKER_COMPOSE run wait_zookeeper
+$DOCKER_COMPOSE up -d mesos_master
+$DOCKER_COMPOSE run wait_mesos_master
+$DOCKER_COMPOSE up -d mesos_slave
+$DOCKER_COMPOSE build wokentest
+$DOCKER_COMPOSE run wait_dbs
+
+echo "Create databases..."
+$DOCKER_COMPOSE run create_dbs
+
+echo "Migrate woken database..."
+$DOCKER_COMPOSE run woken_db_setup
+
+echo "Migrate metadata database..."
+$DOCKER_COMPOSE run sample_meta_db_setup
+
+echo "Migrate features database..."
+$DOCKER_COMPOSE run sample_data_db_setup
+
+echo "Run containers..."
+for i in 1 2 3 4 5 ; do
+  $DOCKER_COMPOSE up -d chronos
+  $DOCKER_COMPOSE run wait_chronos
+  $DOCKER_COMPOSE logs chronos | grep java.util.concurrent.TimeoutException || break
+  echo "Chronos failed to start, restarting..."
+  $DOCKER_COMPOSE stop chronos
+done
+
+echo "Please start Woken from your IDE. It should use port 8087 for Akka cluster"
+read -p "Press enter to continue >"
+
+if [ $validation == 1 ]; then
+
+    $DOCKER_COMPOSE up -d wokenvalidation
+
+    $DOCKER_COMPOSE run wait_woken
+
+    for i in 1 2 3 4 5 ; do
+      $DOCKER_COMPOSE logs chronos | grep java.util.concurrent.TimeoutException || break
+      echo "Chronos failed to start, restarting..."
+      $DOCKER_COMPOSE stop chronos
+      $DOCKER_COMPOSE up -d chronos
+      $DOCKER_COMPOSE run wait_chronos
+    done
+
+fi
+
+if [ $frontend == 1 ]; then
+    echo
+    echo "Now that's up to you to play with the user interface..."
+
+    $DOCKER_COMPOSE up -d portalbackend
+
+    $DOCKER_COMPOSE run wait_portal_backend
+
+    $DOCKER_COMPOSE up -d frontend
+fi
