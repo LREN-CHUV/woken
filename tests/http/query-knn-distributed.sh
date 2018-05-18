4c58f3c31bdb23b125308c90c06ab2eef8dbaa89
Ludovic Claude <ludovic.claude@laposte.net>
2018-05-18 09:46:53 +0200

    [IT] update datasets

--- tests/http/query-knn-distributed.sh
+++ tests/http/query-knn-distributed.sh
@@ -5,6 +5,6 @@ http -v --verify=no -a admin:WoKeN --timeout 180 POST http://localhost:8087/mini
          variables:='[{"code":"brainstem"}]' \
          grouping:='[]' \
          covariables:='[{"code":"leftcaudate"}]' \
-         datasets:='[{"code":"desd-synthdata"},{"code":"nida-synthdata"}]' \
+         datasets:='[{"code":"desd-synthdata"},{"code":"qqni-synthdata"}]' \
          targetTable='cde_features_mixed' \
          algorithm:='{"code":"knn", "name": "KNN", "parameters": []}'
